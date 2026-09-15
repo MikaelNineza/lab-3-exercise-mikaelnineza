9c38de65c0e8951a43587455e563dd48475ed8f4
Mikael-UofA <lnineza@ualberta.ca>
2026-09-15 16:06:09 -0600

    main work completed

--- src/SharedPtr.h
+++ src/SharedPtr.h
@@ -6,7 +6,11 @@ public:
     ControlBlockBase() : refCnt(1) {}
 
     // dtor is virtual, so that we can call derived class's dtor from a ptr to this base class.
-    virtual ~ControlBlockBase(); // TODO: implement the destructor.
+    virtual ~ControlBlockBase() {
+        if (refCnt < 1) {
+            delete this;
+        }
+    }
 
     // pure virtual function; must be overriden by derived classes
     virtual void* managedAddress() = 0;
@@ -33,8 +37,127 @@ public:
     }
 
 private:
-    // TODO: add field(s) which both control block types need to have
     long refCnt;
+};
+
+template <class T>
+class ControlBlock: public ControlBlockBase {
+    public:
+        ControlBlock(T* p = nullptr) : storedPtr(p) {}
+
+        ~ControlBlock() override {
+            delete storedPtr;
+        }
+
+        void* managedAddress() override {
+            return storedPtr;
+        }
+
+    private:
+        T* storedPtr;
+};
+
+template <class T>
+class SharedPtr {
+    public:
+        SharedPtr() : storedPtr(nullptr), cBlock(nullptr) {}
+
+        SharedPtr(T* p = nullptr) : storedPtr(p) {
+            cBlock = new ControlBlock<T>(p);
+        }
+
+        template <typename U> SharedPtr(const SharedPtr<U>& other, T* storedPtr) {
+            this->storedPtr = storedPtr;
+
+        }
+
+        ~SharedPtr() {
+            deletePtr();
+        }
+
+        // copy
+        SharedPtr(const SharedPtr<T>& other) {
+            cBlock = other.cBlock;
+            storedPtr = other.storedPtr;
+            cBlock->increment();
+        }
+
+        // move
+        SharedPtr(SharedPtr<T>&& other) {
+            cBlock = other.cBlock;
+            storedPtr = other.storedPtr;
+            other.reset();
+        }
+        // copy operator
+        SharedPtr<T>& operator=(const SharedPtr<T>& other) {
+            if (this != &other) {
+                reset();
+                cBlock = other.cBlock;
+                storedPtr = other.storedPtr;
+                cBlock->increment();
+            }
+            return *this;
+        };
+        // move operator
+        SharedPtr<T>& operator=(SharedPtr<T>&& other) {
+            if (this != &other) {
+                reset();
+                cBlock = other.cBlock;
+                storedPtr = other.storedPtr;
+                other.reset();
+            }
+            return *this;
+        }
+
+        bool operator==(const SharedPtr<T>& other) const {return storedPtr == other.storedPtr;}
+        T& operator*() const {return *storedPtr;}
+        T* operator->() const {return storedPtr;}
+        operator bool() const {return storedPtr != nullptr;}
+
+        T* get() const { return storedPtr;}
+
+        void reset() {
+            deletePtr();
+        }
+
+        void reset(T* other) {
+            swap(new SharedPtr(other));
+        }
+
+        void swap(SharedPtr<T>& other) {
+            if (this == other) {
+                return;
+            }
+            T* tempT = storedPtr;
+            ControlBlock tempBlock = cBlock;
+            storedPtr = other.storedPtr;
+            cBlock = other.cBlock;
+            other.storedPtr = tempT;
+            other.cBlock = tempBlock;
+        }
+
+        long useCount() {
+            return cBlock->refCount();
+        }
+
+        bool deletePtr() {
+            if (cBlock) {
+                cBlock->decrement();
+                if (cBlock->refCount() < 1) {
+                    delete cBlock;
+                    cBlock = nullptr;
+                    storedPtr = nullptr;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+
+    private:
+        T* storedPtr;
+        ControlBlockBase* cBlock;
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include <cstdlib>
+#include <cassert>
+#include <utility>
 #include "SharedPtr.h"
 
 int main() {
+   //move
+   SharedPtr<int> sPtr1(new int(5));
+   SharedPtr<int> sPtr2(std::move(sPtr1));
+   assert(sPtr1.get() == nullptr);
+   assert(*(sPtr2.get()) == 5);
+   
+   //copy 
+   SharedPtr<int> sPtr3(new int(5));
+   SharedPtr<int> sPtr4 = sPtr1;
+   assert(*(sPtr3.get()) == 5);
+   assert(*(sPtr4.get()) == 5);
    return EXIT_SUCCESS;
 }
